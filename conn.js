066052858a9bf01ca5f5d5ea0051e8781aa8176f
starriolaui-lgtm <star.alado.ui@phinmaed.com>
2026-03-30 00:19:06 +0800

    fix variable

--- conn.js
+++ conn.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql2/promise');
 
 const db = mysql.createPool({
-  host:     process.env.DB_HOST     || 'mysql.railway.internal',
-  user:     process.env.DB_USER     || 'root',
-  password: process.env.DB_PASSWORD || 'WagFdwJemqfYHrOIlEAbZIDDXdROoYOm',
-  database: process.env.DB_NAME     || 'railway',
-  port:     process.env.DB_PORT     || 3306,
+  host:     process.env.MYSQLHOST     || 'mysql.railway.internal',
+  user:     process.env.MYSQLUSER     || 'root',
+  password: process.env.MYSQLPASSWORD || 'WagFdwJemqfYHrOIlEAbZIDDXdROoYOm',
+  database: process.env.MYSQLDATABASE     || 'railway',
+  port:     process.env.MYSQLPORT     || 3306,
   waitForConnections: true,
   connectionLimit: 10,
 });
