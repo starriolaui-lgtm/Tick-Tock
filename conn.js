2ef5276bafd501adbb2c339366fa9a1e520dff23
starriolaui-lgtm <star.alado.ui@phinmaed.com>
2026-03-30 00:05:11 +0800

    variable fix

--- conn.js
+++ conn.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2/promise');
 
 const db = mysql.createPool({
-  host:     process.env.DB_HOST     || 'railway',
+  host:     process.env.DB_HOST     || 'mysql.railway.internal',
   user:     process.env.DB_USER     || 'root',
   password: process.env.DB_PASSWORD || 'WagFdwJemqfYHrOIlEAbZIDDXdROoYOm',
-  database: process.env.DB_NAME     || 'student_task_manager',
+  database: process.env.DB_NAME     || 'railway',
   port:     process.env.DB_PORT     || 3306,
   waitForConnections: true,
   connectionLimit: 10,
