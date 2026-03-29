60fc1faa457d5f273b0a2a5a7139ea3ccb374984
starriolaui-lgtm <star.alado.ui@phinmaed.com>
2026-03-30 00:37:44 +0800

    password fix

--- conn.js
+++ conn.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2/promise');
 const db = mysql.createPool({
   host:     process.env.MYSQLHOST     || 'mysql.railway.internal',
   user:     process.env.MYSQLUSER     || 'root',
-  password: process.env.MYSQLPASSWORD || 'WagFdwJemqfYHrOIlEAbZIDDXdROoYOm',
+  password: process.env.MYSQLPASSWORD || 'GUgzgjodvuRiYYLdJRPLvnhrQdcORwRh',
   database: process.env.MYSQLDATABASE     || 'railway',
   port:     process.env.MYSQLPORT     || 3306,
   waitForConnections: true,
